35039c9d0d3cdf5610c9a605dee5ebec04bdfe74
BartoszCzernal <bartekczernal@gmail.com>
2020-04-22 12:46:31 +0200

    Create Model Author Create Model Category Update Model Book Make Migrations

--- books/models.py
+++ books/models.py
@@ -1,16 +1,26 @@
 from django.db import models
 from isbn_field import ISBNField
-from datetime import date
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=40)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
 
 
 class Book(models.Model):
     isbn = ISBNField(null=True)
     title = models.CharField(max_length=100, null=True)
     date_of_release = models.DateField(null=True)
+    authors = models.ManyToManyField(Author)
+    categories = models.ManyToManyField(Category)
     # on_loan_to : ForeignKey Client
     # owned_by : ForeignKey Library
-    # categories : ManyToMany Category
-    # authors : ManyToMany Authors
+
+
     
     
 
