509d0da33f1553a79385cd58e5c428ee4aff7b3d
BartoszCzernal <bartekczernal@gmail.com>
2020-04-27 14:17:21 +0200

    Restrict Access to sites and elements Create logout template Create profile template

--- books/views.py
+++ books/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from .models import Book
 from django.views.generic import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class BookListView(ListView):
+class BookListView(LoginRequiredMixin, ListView):
     model = Book
     template_name = 'books/home.html'
     context_object_name = 'books'
