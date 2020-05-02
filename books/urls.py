b45368ebbb3f3965a83b4d952081a3ba6e3342d3
pacyna92 <pacyna92@gmail.com>
2020-05-02 21:54:54 +0200

    modify urls path

--- books/urls.py
+++ books/urls.py
@@ -1,14 +1,12 @@
 from django.urls import path
-from . import views
 from django.conf.urls import url
 from django import views as django_views
-from .views import BookListView, BookDetailView, BookCreateView, BookUpdateView, BookDeleteView
+from .views import BookDetailView, BookCreateView, BookUpdateView, BookDeleteView
 
 urlpatterns = [
-    path('', BookListView.as_view(), name='home'),
-    path('book/<int:pk>', BookDetailView.as_view(), name='book-detail'),
-    path('book/new/', BookCreateView.as_view(), name='book-create'),
-    path('book/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),
-    path('book/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),
+    path('<int:pk>', BookDetailView.as_view(), name='book-detail'),
+    path('new/', BookCreateView.as_view(), name='book-create'),
+    path('<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),
+    path('<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),
     url(r'^jsi18n/$', django_views.i18n.JavaScriptCatalog.as_view(), name='jsi18n'),
 ]
